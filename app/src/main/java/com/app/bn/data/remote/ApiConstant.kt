c76627510972c1398e175cabadf9eb4a781c24d5
Shivam <shivam@appventurez.com>
2022-08-28 19:44:38 +0530

    initial commit

--- app/src/main/java/com/app/bn/data/remote/ApiConstant.kt
+++ app/src/main/java/com/app/bn/data/remote/ApiConstant.kt
@@ -2,4 +2,5 @@ package com.app.bn.data.remote
 
 //retrofit constants
 internal const val RETROFIT_SERVICE_TIMEOUT                 = 10 * 60 * 1000L
+
 internal const val AUTHORIZATION                            = "Authorization"
